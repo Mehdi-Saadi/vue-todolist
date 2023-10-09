e3e723f374b598eafec13cf39c73b6720d16fa37
Mehdi <mehdi.0.saadi@gmail.com>
2023-10-09 15:21:38 +0330

    remove task

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,19 @@ const addTask = () => {
     });
 
     new_task.value = '';
+};
+
+const removeTask = task => {
+    tasks.value = tasks.value.filter(t => t !== task)
 }
 
-watch(tasks, (newVal) => {
+watch(tasks, newVal => {
     localStorage.setItem('tasks', JSON.stringify(newVal));
 }, { deep: true });
 
 onMounted(() => {
     tasks.value = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log(tasks.value)
-})
-
+});
 </script>
 
 <template>
@@ -42,8 +44,7 @@ onMounted(() => {
                     type="text"
                     placeholder="e.g. Do exercise"
                     class="w-full py-2 px-3 rounded-lg outline-none mb-4 shadow-lg ring-1 ring-gray-200"
-                    v-model="new_task"
-                >
+                    v-model="new_task">
                 <div class="flex flex-row justify-center space-x-5 select-none">
                     <label for="cat1" class="flex flex-col items-center cursor-pointer rounded-lg shadow-lg w-full py-3 ring-1 ring-gray-200 active:scale-95 space-y-1 bg-white">
                         <input
@@ -52,8 +53,7 @@ onMounted(() => {
                             id="cat1"
                             value="business"
                             class="w-5 h-5"
-                            v-model="input_category"
-                        >
+                            v-model="input_category">
                         <span>Business</span>
                     </label>
                     <label for="cat2" class="flex flex-col items-center cursor-pointer rounded-lg shadow-lg w-full py-3 ring-1 ring-gray-200 active:scale-95 space-y-1 bg-white">
@@ -63,8 +63,7 @@ onMounted(() => {
                             id="cat2"
                             value="personal"
                             class="w-5 h-5 accent-pink-500"
-                            v-model="input_category"
-                        >
+                            v-model="input_category">
                         <span>Personal</span>
                     </label>
                 </div>
@@ -77,20 +76,20 @@ onMounted(() => {
                 <div
                     v-for="(task, index) in tasks"
                     class="rounded-lg shadow-lg bg-white w-full py-2 px-4 flex items-center justify-between"
-                    :class="[(task.done) ? 'line-through' : '']"
-                    :key="index"
-                >
+                    :class="[(task.done) ? 'line-through text-gray-400' : '']"
+                    :key="index">
                     <div class="flex items-center grow">
                         <input
                             type="checkbox"
                             v-model="task.done"
                             class="appearance-none w-4 h-4 border rounded-full bg-white checked:border-white checked:ring-1 mr-2 cursor-pointer"
-                            :class="[task.category === 'business' ? 'border-blue-500 checked:bg-blue-500 checked:ring-blue-500' : 'border-pink-500 checked:bg-pink-500 checked:ring-pink-500']"
-                        >
+                            :class="[task.category === 'business' ? 'border-blue-500 checked:bg-blue-500 checked:ring-blue-500' : 'border-pink-500 checked:bg-pink-500 checked:ring-pink-500']">
                         <input type="text" v-model="task.content" class="outline-none grow cursor-pointer" spellcheck="false">
                     </div>
 
-                    <button class="text-red-700 rounded-lg hover:bg-gray-100 transition duration-150 p-2">
+                    <button
+                        class="text-red-700 rounded-lg hover:bg-gray-100 transition duration-150 p-2"
+                        @click="removeTask(task)">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                         </svg>
